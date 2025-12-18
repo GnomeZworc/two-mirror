05b3827e3cdd6c48c3a6ac02699904f833167fe6
GnomeZworc <nicolas.boufidjeline@g3e.fr>
2025-12-18 14:09:37 +0100

    f-1: fix: fix forgeten args #1
    Signed-off-by: GnomeZworc <nicolas.boufidjeline@g3e.fr>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,12 +24,16 @@ check_latest_script () {
 }
 
 main () {
+    echo "$@"
     [[ -f ./libs/shflags ]] && . ./libs/shflags || eval "$(curl --silent https://git.g3e.fr/H6N/tools/raw/branch/main/libs/shflags)"
 
     DEFINE_boolean 'dryrun'     false                 'Enable dry-run mode' 'd'
     DEFINE_string  'git_server' 'https://git.g3e.fr/' 'Git Server'          'g'
     DEFINE_string  'repo_path'  'syonad/two/'         'Path of repository'  'r'
     DEFINE_string  'branch'     'main/'               'Branch name'         'b'
+
+    FLAGS "$@" || exit $?
+    eval set -- "${FLAGS_ARGV}"
 
     check_latest_script "${FLAGS_git_server}${FLAGS_repo_path}raw/branch/${FLAGS_branch}${SCRIPT_PATH}" "${0}"
 }
