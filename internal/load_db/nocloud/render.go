37e345609e58307487af4e616aa14ca1834680fd
GnomeZworc <nicolas.boufidjeline@g3e.fr>
2026-01-07 21:09:04 +0100

    f-11: fix: clean error in nocloud work #11
    Signed-off-by: GnomeZworc <nicolas.boufidjeline@g3e.fr>

--- internal/load_db/nocloud/render.go
+++ internal/load_db/nocloud/render.go
@@ -26,25 +26,21 @@ func renderConfig(path string, cfg Config) (string, error) {
 	return buf.String(), nil
 }
 
-var DB *badger.DB
-
 func LoadNcCloudInDB(config Config, db *badger.DB) {
 	meta_data, _ := renderConfig("templates/meta-data.tmpl", config)
 	user_data, _ := renderConfig("templates/user-data.tmpl", config)
 	network_config, _ := renderConfig("templates/network-config.tmpl", config)
 	vendor_data, _ := renderConfig("templates/vendor-data.tmpl", config)
 
-	DB = db
-
-	kv.AddInDB(DB, "metadata/"+config.Name+"/meta-data", meta_data)
-	kv.AddInDB(DB, "metadata/"+config.Name+"/user-data", user_data)
-	kv.AddInDB(DB, "metadata/"+config.Name+"/network-config", network_config)
-	kv.AddInDB(DB, "metadata/"+config.Name+"/vendor-data", vendor_data)
-	kv.AddInDB(DB, "metadata/"+config.Name+"/vpc", config.VpcName)
-	kv.AddInDB(DB, "metadata/"+config.Name+"/bind_ip", config.BindIP)
-	kv.AddInDB(DB, "metadata/"+config.Name+"/bind_port", config.BindPort)
+	kv.AddInDB(db, "metadata/"+config.Name+"/meta-data", meta_data)
+	kv.AddInDB(db, "metadata/"+config.Name+"/user-data", user_data)
+	kv.AddInDB(db, "metadata/"+config.Name+"/network-config", network_config)
+	kv.AddInDB(db, "metadata/"+config.Name+"/vendor-data", vendor_data)
+	kv.AddInDB(db, "metadata/"+config.Name+"/vpc", config.VpcName)
+	kv.AddInDB(db, "metadata/"+config.Name+"/bind_ip", config.BindIP)
+	kv.AddInDB(db, "metadata/"+config.Name+"/bind_port", config.BindPort)
 }
 
 func UnLoadNoCloudInDB(vm_name string, db *badger.DB) {
-	kv.DeleteInDB(DB, "metadata/"+vm_name)
+	kv.DeleteInDB(db, "metadata/"+vm_name)
 }
