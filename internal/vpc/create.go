2ea3c6dd14ba65e99e902747356d608b422ef4e5
GnomeZworc <nicolas.boufidjeline@g3e.fr>
2026-01-19 22:41:39 +0100

    f-14: fix: rename interface name
    Signed-off-by: GnomeZworc <nicolas.boufidjeline@g3e.fr>

--- internal/vpc/create.go
+++ internal/vpc/create.go
@@ -16,42 +16,36 @@ func CreateVPC(name string) error {
 	}
 
 	// create veth public for this netns
-	if err := netif.CreateVethToNetns(name+"-ext", "veth-"+name+"-int", "/var/run/netns/"+name, 9000); err != nil {
+	if err := netif.CreateVethToNetns("veth"+name+"ext", "vethpublicint", "/var/run/netns/"+name, 9000); err != nil {
 		return err
 	}
 
 	// create public bridge in netns
 	if err := netns.Call(name, func() error {
-		return netif.CreateBridge("br-public", 1500, false)
+		return netif.CreateBridge("br-public", 1500)
 	}); err != nil {
 		return err
 	}
 
 	// set veth to ext public bridge
-	if err := netif.BridgeSetMaster(name+"-ext", "br-public"); err != nil {
+	if err := netif.BridgeSetMaster("veth"+name+"ext", "br-public"); err != nil {
 		return err
 	}
 
 	// set veth to int public bridge
 	if err := netns.Call(name, func() error {
-		return netif.BridgeSetMaster("veth-"+name+"-int", "br-public")
+		return netif.BridgeSetMaster("vethpublicint", "br-public")
 	}); err != nil {
 		return err
 	}
 
 	// set set ext veth up
-	if err := netif.LinkSetUp(name + "-ext"); err != nil {
+	if err := netif.LinkSetUp("veth" + name + "ext"); err != nil {
 		return nil
 	}
 	// set set int veth up
 	if err := netns.Call(name, func() error {
-		return netif.LinkSetUp("veth-" + name + "-int")
-	}); err != nil {
-		return err
-	}
-	// set set int bridge up
-	if err := netns.Call(name, func() error {
-		return netif.LinkSetUp("br-public")
+		return netif.LinkSetUp("vethpublicint")
 	}); err != nil {
 		return err
 	}
