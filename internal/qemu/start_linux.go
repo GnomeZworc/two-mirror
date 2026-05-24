a0637d827ad16cda9457e8085221af29e59d205d
GnomeZworc <nicolas.boufidjeline@g3e.fr>
2026-05-24 16:13:11 +0200

    f-32: syntax: fix duplicated code
    Signed-off-by: GnomeZworc <nicolas.boufidjeline@g3e.fr>

--- internal/qemu/start_linux.go
+++ internal/qemu/start_linux.go
@@ -11,25 +11,6 @@ import (
 	"strings"
 )
 
-type DiskConfig struct {
-	Path string
-	Dev  string
-}
-
-type Config struct {
-	Name         string
-	TapID        int
-	Mac          string
-	Disks        []DiskConfig
-	Memory       int
-	CPUs         int
-	UEFICodePath string
-	UEFIVarsPath string
-	SerialDir    string
-	MonitorDir   string
-	QMPDir       string
-}
-
 func Start(cfg Config) error {
 	memory := cfg.Memory
 	if memory == 0 {
--- internal/qemu/start_other.go
+++ internal/qemu/start_other.go
@@ -2,26 +2,9 @@
 
 package qemu
 
-import "errors"
-
-type DiskConfig struct {
-	Path string
-	Dev  string
-}
-
-type Config struct {
-	Name         string
-	TapID        int
-	Mac          string
-	Disks        []DiskConfig
-	Memory       int
-	CPUs         int
-	UEFICodePath string
-	UEFIVarsPath string
-	SerialDir    string
-	MonitorDir   string
-	QMPDir       string
-}
+import (
+	"errors"
+)
 
 func Start(_ Config) error {
 	return errors.New("vm: not supported on this platform")
