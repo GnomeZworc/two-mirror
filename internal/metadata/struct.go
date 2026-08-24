d15454d35b7054f1c88d1cf4f0fe9d1cf68ebd43
GnomeZworc <nicolas.boufidjeline@g3e.fr>
2026-08-24 22:57:30 +0200

    f-34: route: add test for route metadata #34
    Signed-off-by: GnomeZworc <nicolas.boufidjeline@g3e.fr>

--- internal/metadata/struct.go
+++ internal/metadata/struct.go
@@ -1,5 +1,8 @@
 package metadata
 
+// ServiceIP is the well-known address guests use to reach the metadata server.
+// Traffic to it is DNATed to the subnet gateway by internal/iptables, and the
+// route to it is advertised by internal/dhcp.
 const ServiceIP = "169.254.169.254"
 
 type NoCloudData struct {
