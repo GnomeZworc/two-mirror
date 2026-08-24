52d214404d7c4699a5ddcbf19fc0a54c90e18acb
GnomeZworc <nicolas.boufidjeline@g3e.fr>
2026-08-24 21:18:52 +0200

    f-43: dhcp: fix default route create for nothing #43
    Signed-off-by: GnomeZworc <nicolas.boufidjeline@g3e.fr>

--- internal/dhcp/struct.go
+++ internal/dhcp/struct.go
@@ -10,7 +10,7 @@ type Config struct {
 	Network        *net.IPNet
 	VPCGateway     net.IP     // next-hop for VPCRoute (option 121)
 	VPCRoute       *net.IPNet // if non-nil, emit dhcp-option=121,VPCRoute,VPCGateway
-	DefaultGateway net.IP     // if non-nil, emit dhcp-option=3,DefaultGateway
+	DefaultGateway net.IP     // if non-nil, emit dhcp-option=3,DefaultGateway; if nil, emit a bare dhcp-option=3 to suppress the dnsmasq default
 	Name           string
 	ConfDir        string
 }
